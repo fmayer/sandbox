9e57dad4d4c00a85a154265216947e1117b1bb75
Florian Mayer <flormayer@aim.com>
2010-09-15 01:09:24 +0200

    Changes to oldlookup; add license headers.

--- oldlookup.py
+++ oldlookup.py
@@ -1,11 +1,35 @@
-# (C) 2010 by Florian Mayer.
+# Copyright (c) 2010 Florian Mayer
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 
 import inspect
 
-magic = set(('__int__',))
+forbidden = set(('__setattr__', '__class__'))
+
+def is_magic(name):
+    return (
+        name not in forbidden and name.startswith('__') and name.endswith('__')
+    )
+
 
 def oldstyle(self, name, attr):
-    if name in magic:
+    if is_magic(name):
         d = dict(self.__class__.__dict__)
         d.update({name: staticmethod(attr)})
         ncls = type(
@@ -33,8 +57,10 @@ class A(object):
 
 a = A()
 print int(a)
+assert int(a) == 2
 a.__int__ = lambda: 5
 print int(a)
+assert int(a) == 5
 
 def sattr(name, attr):
     print name, attr
@@ -55,5 +81,7 @@ class B(object):
 
 b = B()
 print int(b)
+assert int(b) == 2
 b.__int__ = lambda: 5
 print int(b)
+assert int(b) == 2
