a1196f83a4054949c87a70e16db7bae35c824238
Florian Mayer <flormayer@aim.com>
2010-10-01 21:02:48 +0200

    Add decimal slice.

---
+++ decslice.py
@@ -0,0 +1,35 @@
+# Copyright (c) 2010 Florian Mayer <flormayer (at) aim (dot) com>
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+import math
+
+def nth(x, n, digits=1):
+    if n < 0:
+        return int(x / (10 ** (abs(n) - 1))) % 10 ** digits
+    else:
+        return int(x / (10 ** int(math.log10(x) - n))) % 10 ** digits
+
+
+def slice(x, i, j):
+    if i < 0 and j >= 0:
+        i = int(math.log10(x)) + 2 + i
+    elif j < 0 and i >= 0:
+        j = int(math.log10(x)) + 2 + j
+    return nth(x, max(i, j - 1), abs(i - j))
